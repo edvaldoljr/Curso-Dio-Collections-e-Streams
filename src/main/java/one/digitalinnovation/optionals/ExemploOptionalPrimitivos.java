ef5499142d89ebd6cf6457036a10af6eabcbcec5
edvaldoljr <edvaldoluizj@gmail.com>
2021-10-29 11:39:02 -0300

    ExemploOptionalPrimitivos

---
+++ src/main/java/one/digitalinnovation/optionals/ExemploOptionalPrimitivos.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.optionals;
+
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.OptionalLong;
+
+public class ExemploOptionalPrimitivos {
+
+    public static void main(String[] args) {
+
+        System.out.println("Valor inteiro opcional");
+        OptionalInt.of(12).ifPresent(System.out::println);
+
+        System.out.println("Valor decimal opcional");
+        OptionalDouble.of(55.2).ifPresent(System.out::println);
+
+        System.out.println("Valor longo opcional");
+        OptionalLong.of(23l).ifPresent(System.out::println);
+    }
+}
